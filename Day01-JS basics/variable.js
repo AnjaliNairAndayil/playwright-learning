1751c7f8fdfe0298d3442a62fbe4680d379c94a5
anjaliandayil <anjali.andayil@infomind.ai>
2026-07-16 16:38:45 +0530

    added comment

--- Day01-JS basics/variable.js
+++ Day01-JS basics/variable.js
@@ -1,5 +1,15 @@
-let name='Anjali';
-console.log(name);
+let name1='VIjay';
+console.log(name1);
 
-let age='25';
+let age1=25;
+console.log(age1);
+
+let name ='Anjali';
+let age  = 41;
+let city ='Bangalore';
+let islearning='yes';
+
+console.log(name);
 console.log(age);
+console.log(city);
+console.log(islearning);
