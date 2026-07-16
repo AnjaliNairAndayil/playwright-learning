78b4e6b2f1da60b4043d2140765918c203825ea2
anjaliandayil <anjali.andayil@infomind.ai>
2026-07-16 16:51:12 +0530

    changed type

--- Day01-JS basics/practise.js
+++ Day01-JS basics/practise.js
@@ -8,3 +8,16 @@ console.log(firstname);
 console.log(favoritefood);
 console.log(favoritenumber);
 console.log(likesautomation);
+
+/*Practise 2*/
+
+let company ='OpenAi';
+let experience=2;
+let salary =2000;
+let remote=true;
+
+console.log(company);
+console.log(experience);
+console.log(salary);
+console.log(remote);
+
