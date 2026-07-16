978d5044cf15be184e6519b24c5269eeb9a905c4
anjaliandayil <anjali.andayil@infomind.ai>
2026-07-16 16:46:01 +0530

    practise 1

---
+++ Day01-JS basics/practise.js
@@ -0,0 +1,10 @@
+/* pracitice 1*/
+let firstname ='Anjali';
+let favoritefood='Idli';
+let favoritenumber='10';
+let likesautomation='Yes';
+
+console.log(firstname);
+console.log(favoritefood);
+console.log(favoritenumber);
+console.log(likesautomation);
