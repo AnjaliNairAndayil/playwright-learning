a37ce7a17e4c9b3511667db279fed470a1ba4a04
anjaliandayil <anjali.andayil@infomind.ai>
2026-07-17 10:30:47 +0530

    comparison.js added

---
+++ Day01-JS basics/comparision.js
@@ -0,0 +1,16 @@
+//The output predictions
+
+
+
+let a='10';
+let b=10;
+let c=20;
+
+console.log(a==b); /*true*/
+console.log(a===b); /*false*/
+console.log(c>b); /*true*/
+console.log(c<b); /*false*/
+console.log(c>=20); /*true*/
+console.log(c<=10); /*false*/
+console.log(a!=b); /*false*/
+console.log(a!==b); /*true*/
