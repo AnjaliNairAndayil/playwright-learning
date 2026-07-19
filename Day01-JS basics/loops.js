e8b88413303380467d676980385292b47c2ad9f5
anjaliandayil <anjali.andayil@infomind.ai>
2026-07-19 14:51:24 +0530

    Loops

---
+++ Day01-JS basics/loops.js
@@ -0,0 +1,36 @@
+// example 1
+
+for(let i=1;i<=5;i++)
+{
+    console.log("Hello");
+}
+
+//Example 2
+
+for(let i=1; i<=10;i++)
+{
+    console.log(i);
+}
+
+//Example 3 (even number)
+
+for (let i=0; i<=10;i+=2)
+{
+    console.log(i);
+}
+
+//Example 4 (countdown)
+
+for (let i=5;i>=0;i--)
+{
+    console.log(i);
+}
+
+//Example 5 (Multiplication table)
+
+let number =5;
+
+for (let i=1;i<=10;i++)
+{
+    console.log(number +"*" + i +"="+ (number*i));
+}
---
+++ Day01-JS basics/loops_practise.js
@@ -0,0 +1,45 @@
+//Practise 1
+
+for (let i=1; i<=5;i++)
+{
+    console.log(i)
+}
+
+//practise 2
+
+for (let i=10; i>=6; i--)
+{
+    console.log(i);
+}
+
+//practise 3
+
+for (let i =1 ; i<=10; i+=3)
+{
+    console.log(i);
+}
+
+//challenge (print 20 to 30)
+
+for (let i=20;i<=30;i++)
+{
+    console.log(i);
+}
+
+//challenge (print odd from 1 to 20)
+
+for (let i=1;i<=20;i++)
+{
+    if (i%2==1){
+        console.log(i);
+    }
+}
+
+//challenge (print sum of numbers 1 to 10)
+
+sum= 0
+for (let i=1;i<=10;i++)
+{
+    sum+=i;
+    console.log(sum);
+}
