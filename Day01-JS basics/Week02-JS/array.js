687419dbd13f2f8f90b0b30aa5966e28e6371362
anjaliandayil <anjali.andayil@infomind.ai>
2026-07-21 23:13:49 +0530

    arrays

---
+++ Day01-JS basics/Week02-JS/array.js
@@ -0,0 +1,15 @@
+// array examples
+
+let products=['laptop','mouse','keyboard','mobile']
+
+for (let i=0;i<products.length;i++)
+{
+    console.log(products[i]);
+}
+
+//mixed array
+let student = ["Anjali", 25, true];
+
+console.log(student[2]);
+student[2]="Study";
+console.log(student[2]);
