b5e8d4bea519f8b42d79f9821ae86966c9e04da1
anjaliandayil <anjali.andayil@infomind.ai>
2026-07-21 22:56:37 +0530

    moved to a folder

---
+++ Day01-JS basics/Week01-JS/Mini_Project.js
@@ -0,0 +1,13 @@
+//Mini Project: Build a simple calculator in JavaScript.
+
+const add=(a,b)=>a+b
+const sub=(a,b)=>a-b;
+const mul=(a,b)=>a*b;
+const div=(a,b)=>a/b;
+const mod=(a,b)=>a%b;
+
+console.log(add(2,4));
+console.log(sub(3,1));
+console.log(mul(40,3));
+console.log(div(20,5));
+console.log(mod(20,4));
